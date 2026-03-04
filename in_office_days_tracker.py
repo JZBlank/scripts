e81b4395494d3e9831c06e63d530dfb9d45fc114
joyce <joycezhang2065@gmail.com>
2026-03-03 20:28:59 -0500

    Update in-person office day

--- in_office_days_tracker.py
+++ in_office_days_tracker.py
@@ -61,16 +61,18 @@ def main():
     
     days_left = calculate_valid_days_left(current_date=current_date, end_date=end_date)
     
-    went_in_office = {1:[22,27,28,29], 2:[3,4,5,10,11,12,18,19,26], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[], 11:[], 12:[]}
+    went_in_office = {1:[22,27,28,29], 2:[3,4,5,10,11,12,18,19,26], 3:[3], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[], 11:[], 12:[]}
 
     for i in range(1, 13):
         print(highlight_dates(year, i, went_in_office))
     
+    went_in_office_count = sum(len(v) for v in went_in_office.values())
+
     print("---------------------------------------------------------------------------------------------------")
     print("Summary:")
     print(f'There are {days_left} out of {total_valid_days_in_year} valid days left in the year.')
-    print(f'Total in-person office days I currently have vs needed: {len(went_in_office)}/110')
-    print(f'I need {(110 - len(went_in_office))} days left which is under {days_left}.')
+    print(f'Total in-person office days I currently have vs needed: {went_in_office_count}/110')
+    print(f'I need {(110 - went_in_office_count)} days left which is under {days_left}.')
     print("There are 6 upcoming holidays that may or may not fall under one of those days")
 
 main()
